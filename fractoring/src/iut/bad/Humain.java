6a18a471c8e4228153b44dce46f2e072e91db51f
Houlder <Houlder@DESKTOP-PCRDHOO>
2024-04-02 22:48:18 +0300

    cinq commite

--- fractoring/src/iut/bad/Humain.java
+++ fractoring/src/iut/bad/Humain.java
@@ -32,4 +32,7 @@ public class Humain {
 	public void details() {
 		System.out.println("Nom : " + nom + "\nPrenom : " + prenom + "\nAge : " + age);
 	}
+	public void ami(Humain h) {
+		System.out.println(this.nom + " "+this.prenom+ " est ami avec " + h.nom+ " "+h.prenom );
+	}
 }
